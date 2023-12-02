b45a12981aadeb79431b6d09ec00fbce92753ba3
triple501501 <triple501.silver@gmail.com>
2023-12-02 16:41:57 +0900

    change java

--- src/main/java/com/example/cloudnative/CloudnativeApplication.java
+++ src/main/java/com/example/cloudnative/CloudnativeApplication.java
@@ -3,16 +3,21 @@ package com.example.cloudnative;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 @SpringBootApplication
 public class CloudnativeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CloudnativeApplication.class, args);
-	}
-
 	@RequestMapping("/")
 	String home() {
-		return "Hello World34!!!!!!ddd";
+		return "Hello World34!!!!!!";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CloudnativeApplication.class, args);
 	}
 
 }
